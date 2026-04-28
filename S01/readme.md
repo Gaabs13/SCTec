364123354c688db22bc308aaefccf76b85ed8518
Gabriel Carlos de Souza <contatogabsc@gmail.com>
2026-04-28 19:56:39 -0300

    organização

---
+++ S01/readme.md
@@ -0,0 +1,16 @@
+# Aulas SCTEC
+Conteúdo com as aulas do SCTEC 2026
+
+# Conteudos
+
+Semana 01:
+- Trello
+- Github e Github Desktop
+- VS Code
+- Lógica de Programação
+- JS
+
+Semana 02:
+- JS
+- Variáveis
+- Funções
---
+++ S02/Aulas/Aula 01/main.js
@@ -0,0 +1,20 @@
+se estiver "errado", fica assim
+
+import assimTaCerto from './assimTaCerto.js';
+
+console.log(assimTaCerto);
+
+export default 'assimTaCorretoTb';
+
+assimTaCorretoTb
+
+
+
+// tem momento que tu usa assistente(IA) e ele "fecha novamente" pra te indicar, as vezes tu copia e da erro por isso
+
+// git add . e depois git push origin main = fazendo o upload do código pro github, ai tu pode acessar de qualquer lugar, e compartilhar com outras pessoas, ou até mesmo usar pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou pra pedir ajuda, ou pra mostrar pra alguém o que tu fez, ou para pedir ajuda. //
+
+//git commit -m "mensagem do commit" = fazendo o commit, ou seja, salvando as mudanças que tu fez no código, e a mensagem do commit é uma descrição do que tu fez, pra quando tu for olhar o histórico de commits, tu saber o que tu fez em cada commit. //
+
+//não recebi nenhuma avaliação negativa, mas ouvi muita história mal contada.
+
